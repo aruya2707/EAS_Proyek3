4cff4a44872cd16e125f553c6234c0c050d91bbc
Alya_Angraini <116321705+aruya2707@users.noreply.github.com>
2023-11-29 09:29:37 +0700

    awal

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BarangController;
 use Illuminate\Http\Request;
+use Illuminate\Routing\RouteRegistrar;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/barang', [BarangController::class, 'index']);
+// Route::post('/barang/insert', [BarangController::class, 'store']);
+// Route::get('/barang/{kode_barang}', [BarangController::class, 'show']);
+// Route::put('/barang/update/{kode_barang}', [BarangController::class, 'update']);
+// Route::delete('/barang/delete/{kode_barang}', [BarangController::class, 'destroy']);
